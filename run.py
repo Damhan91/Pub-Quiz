947a68d2555f58c67d60073701888d91174a98e4
Damhan91 <damhanp@gmail.com>
2021-10-25 15:14:34 +0000

    add dictionart of questions and also list of answers

--- run.py
+++ run.py
@@ -1,3 +1,41 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def quiz_game():
+    pass
+
+def check_answer():
+    pass
+
+def score():
+    pass
+
+def play_again():
+    pass
+
+
+
+
+#dictionary for the question with key value pairs
+questions = {
+    "What is the national animal of Australia?: ": "c",
+    "What football team in England is known as the Red Devils?: ": "a",
+    "Which team does the formula One driver Lewis Hamilton drive for?: ": "d",
+    "What is the national flower of Japan?: ": "c",
+    "What is the longest river in the world?: ": "c",
+    "Which artist painted the ceiling of the Sistine Chapel in Rome?: ": "b",
+    "When was Netflix founded: ": "c",
+    "In the Avengers, what is Thor Hammer called?: ": "d",
+    "Out of all cryptocurrency’s, which is the most expensive?: ": "a",
+    "Which animal is the largest in the world?: ": "b",
+}
+
+
+
+answers = [["(a)Koala", "(b)Crocodile", "(c)Kangaroo", "(d)Dingo"],
+    ["(a)Manchester Utd", "(b)Liverpool", "(c)Chelsea", "(d)Southhampton"],
+    ["(a)RedBull", "(b)Williams", "(c)Ferrari", "(d)Mercedes"],
+    ["(a)Rose", "(b)Daisy", "(c)Cherry Blossom", "(d)Tulip"],
+    ["(a)Liffey", "(b)Thames", "(c)Nile", "(d)Amazon"],
+    ["(a)Van Gogh", "(b)Michelangelo", "(c)Da Vinci", "(d)Johannes Vermeer"],
+    ["(a)2000", "(b)2020", "(c)1997", "(d)2005"],
+    ["(a)Needle", "(b)Stormbreaker", "(c)The Hammer", "(d)Mjölnir"],
+    ["(a)BitCoin", "(b)Ethereum", "(c)Dogecoin", "(d)Litcoin"],
+    ["(a)Elephant", "(b)Blue Whale", "(c)Killer Whale", "(d)Giraffe"]]
