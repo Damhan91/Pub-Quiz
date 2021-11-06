305abed973d933357be0f3ed31d7d7d75b1d70da
Damhan91 <damhanp@gmail.com>
2021-11-06 13:42:05 +0000

    add more questions

--- test.py
+++ test.py
@@ -22,9 +22,9 @@ def quiz_game():
     
     print("...................")
 
-    question3 = print("What football team in England is known as the Red Devils?")
-    guess = input("(a)Manchester Utd, (b)Liverpool,(c)Chelsea,(d)Southhampton: ")
-    if guess == "a" or guess == "Manchester Utd":
+    question3 = print("Which team does the formula One driver Lewis Hamilton drive for?")
+    guess = input("(a)RedBull,(b)Williams,(c)Ferrari,(d)Mercedes: ")
+    if guess == "d" or guess == "Mercedes":
         score = score + 1
         print("You are correct, you scored 1 point")
     else:
@@ -32,9 +32,9 @@ def quiz_game():
     
     print("...................")
 
-    question4 = print("What football team in England is known as the Red Devils?")
-    guess = input("(a)Manchester Utd, (b)Liverpool,(c)Chelsea,(d)Southhampton: ")
-    if guess == "a" or guess == "Manchester Utd":
+    question4 = print("What is the national flower of Japan?")
+    guess = input("(a)Rose,(b)Daisy,(c)Cherry Blossom,(d)Tulip: ")
+    if guess == "c" or guess == "Cherry Blossom":
         score = score + 1
         print("You are correct, you scored 1 point")
     else:
@@ -42,18 +42,31 @@ def quiz_game():
     
     print("...................")
 
-    question5 = print("What football team in England is known as the Red Devils?")
-    guess = input("(a)Manchester Utd, (b)Liverpool,(c)Chelsea,(d)Southhampton: ")
-    if guess == "a" or guess == "Manchester Utd":
+    question5 = print("What is the longest river in the world?")
+    guess = input("(a)Liffey,(b)Thames,(c)Nile,(d)Amazon: ")
+    if guess == "c" or guess == "Nile":
         score = score + 1
         print("You are correct, you scored 1 point")
     else:
         print("You are incorrect, your scored 0")
+        
+        print("...................")
+
+    question6 = print("Which artist painted the ceiling of the Sistine Chapel in Rome?")
+    guess = input("(a)Van Gogh,(b)Michelangelo,(c)Da Vinci,(d)Johannes Vermeer: ")
+    if guess == "b" or guess == "Michelangelo":
+        score = score + 1
+        print("You are correct, you scored 1 point")
+    else:
+        print("You are incorrect, your scored 0")
+
+
+
 
 
     if score >= 0:
         print("EXCELLENT!, you scored" + " " + str(score) + " " + "out of 10")
-
+    
 
 
 def play_again():
