1c4c28f725db7c3e1f8d48306bbc9b856762b82d
Damhan91 <damhanp@gmail.com>
2021-11-06 13:50:51 +0000

    finished adding question

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ def quiz_game():
     else:
         print("You are incorrect, your scored 0")
         
-        print("...................")
+    print("...................")
 
     question6 = print("Which artist painted the ceiling of the Sistine Chapel in Rome?")
     guess = input("(a)Van Gogh,(b)Michelangelo,(c)Da Vinci,(d)Johannes Vermeer: ")
@@ -60,12 +60,53 @@ def quiz_game():
     else:
         print("You are incorrect, your scored 0")
 
+    print("...................")
+
+    question7 = print("When was Netflix founded?")
+    guess = input("(a)2000, (b)2020, (c)1997, (d)2005: ")
+    if guess == "c" or guess == "1997":
+        score = score + 1
+        print("You are correct, you scored 1 point")
+    else:
+        print("You are incorrect, your scored 0")
+
+    print("...................")
+
+    question8 = print("In the Avengers, what is Thor Hammer called?")
+    guess = input("(a)Needle,(b)Stormbreaker,(c)The Hammer,(d)Mjölnir: ")
+    if guess == "d" or guess == "Mjölnir":
+        score = score + 1
+        print("You are correct, you scored 1 point")
+    else:
+        print("You are incorrect, your scored 0")
+
+    print("...................")
+
+    question9 = print("Out of all cryptocurrency's, which is the most expensive?")
+    guess = input("(a)Bitcoin,(b)Dogecoin,(c)Ethereum,(d)Litcoin: ")
+    if guess == "a" or guess == "Bitcoin":
+        score = score + 1
+        print("You are correct, you scored 1 point")
+    else:
+        print("You are incorrect, your scored 0")
+
+    print("...................")
+
+    question10 = print("Which animal is the largest in the world?)
+    guess = input("(a)Elephant,(b)Blue Whale,(c)Killer Whale,(d)Giraffe: ")
+    if guess == "b" or guess == "Blue Whale":
+        score = score + 1
+        print("You are correct, you scored 1 point")
+    else:
+        print("You are incorrect, your scored 0")
 
 
-
-
-    if score >= 0:
+    if score >= 8:
         print("EXCELLENT!, you scored" + " " + str(score) + " " + "out of 10")
+    elif score >= 5 and < 8:
+    print("Good Job, you scored" + " " + str(score) + " " + "out of 10")
+    else score <5:
+    print("Ohhhh you might want to use google, you scored" + " " + str(score) + " " + "out of 10")
     
 
 
